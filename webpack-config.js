0f78a8084ab2a2b98d0231d6e9b93801e69efef5
Nathan Gonsalves <iamkaprekar@gmail.com>
2023-08-19 10:11:10 -0400

    Initial setup and file directories laid out

---
+++ webpack-config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  },
+  entry: path.join(__dirname, 'client/index.js'),
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+      {
+        test: /.s?[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'), 
+    }), // maybe a problem-child here
+  ],
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+};
