9aa9ca7418654c3353cccc6c8cad0b4f7deb051b
Nathan Gonsalves <iamkaprekar@gmail.com>
2023-08-21 11:04:19 -0400

    Added route for /cat, added to cat model

--- server/models/catModel.js
+++ server/models/catModel.js
@@ -5,6 +5,10 @@ const catSchema = new Schema({
   name: {type: String, required: true},
   lastFed: {type: Date, default: Date.now()},
   feedingLog: {type: Array, default: []},
+  hungry: {type: Boolean, default: true},
+  healthy: {type: Boolean, default: true},
+  clean: {type: Boolean, default: true},
+  groomed: {type: Boolean, default: true},
 })
 
 module.exports = mongoose.model('Cat', catSchema);
