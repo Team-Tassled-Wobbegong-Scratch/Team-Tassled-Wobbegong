79ab59e2839985a30be52e71479ad2a4beb9ef13
Nathan Gonsalves <iamkaprekar@gmail.com>
2023-08-21 14:09:21 -0400

    Fixed database

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 const { default: mongoose } = require('mongoose');
 
 dbConnect = () => {
-  // mongoose.connect('mongodb+srv://iamkaprekar:5o6gNZvaMtaiXqCv@cluster0.bxo9zd6.mongodb.net/?retryWrites=true&w=majority');
-  mongoose.connect('mongodb://localhost:27017');
+  mongoose.connect('mongodb+srv://iamkaprekar:5o6gNZvaMtaiXqCv@cluster0.bxo9zd6.mongodb.net/?retryWrites=true&w=majority');
+  // mongoose.connect('mongodb://localhost:27017');
 };
 
 module.exports = dbConnect;
